ec95b2baf262ce5911ca6534500654759a6c377a
georg-getz <georgi.getz@outlook.com>
2022-04-27 15:38:36 +0300

    debugging

--- src/commands/add.js
+++ src/commands/add.js
@@ -38,11 +38,11 @@ module.exports = {
     let protocol = Protocol.fromDataSources(dataSourcesAndTemplates)
 
     let manifest = await Subgraph.load('subgraph.yaml', {protocol: protocol})
-    console.log([...manifest.entries()]);
+    console.log(manifest._root.entries);
 
     console.log([...manifest.keys()]);
 
-    console.log([...manifest.values()]);
+    // console.log([...manifest.values()]);
     // console.log(manifest)
     // Show help text if requested
     if (help || h) {
