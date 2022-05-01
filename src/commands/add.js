fbca77e89b3293d2d393bbe70d0c907425d47dfc
georg-getz <georgi.getz@outlook.com>
2022-05-02 02:26:52 +0300

    debugging

--- src/commands/add.js
+++ src/commands/add.js
@@ -47,8 +47,8 @@ module.exports = {
     let ds = manifest.result.get('dataSources')
     // console.log(ds)
     console.log(ds.get(0).get('kind') + '\n' + ds.get(0).get('source') + '\n' + ds.get(0).get('mapping'))
-    let wat = Map.of(await addDatasource2(ds.get(0).get('kind'), 
-      'PogO', 'mainnet', ds.get(0).get('source'), ds.get(0).get('mapping'))).toJS()
+    let wat = Map.of(await addDatasource2(ds.get(0).get('kind').toString(), 
+      'PogO', 'mainnet', ds.get(0).get('source').toString(), ds.get(0).get('mapping')).toString()).toJS()
       console.log('wat ' + wat)
     manifest.result.get('dataSources').push(wat)
     // let compiledSubgraph = await Compiler.compileSubgraph(manifest)
