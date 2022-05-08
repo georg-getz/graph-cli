a304254b0f23fbb7f427f71ebdfa7d0e61e2501f
georg-getz <georgi.getz@outlook.com>
2022-05-08 23:21:05 +0300

    debugging

--- src/commands/add.js
+++ src/commands/add.js
@@ -70,7 +70,7 @@ module.exports = {
     for (let i = 0; i < dataSourcesAndTemplates.length; i++) {
       let datasource = dataSourcesAndTemplates[i]
       console.log('datasource: ' + datasource)
-      list.concat(datasource.get('mapping').get('entities'))
+      list.concat(datasource.mapping.entities)
 
     }
     console.log(list)
