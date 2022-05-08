ee8f64fd020526610a72151587bd9d4ed736dde9
georg-getz <georgi.getz@outlook.com>
2022-05-08 04:42:37 +0300

    debugging

--- src/commands/add.js
+++ src/commands/add.js
@@ -70,7 +70,7 @@ module.exports = {
     if (abi) {
       ethabi = EthereumABI.load(contractName, abi)
       // if (!mergeEntities) {
-        ethabi.data.filter(item => item.get('type') === 'event').forEach(event => {
+        ethabi.data.asMutable().filter(item => item.get('type') === 'event').forEach(event => {
           console.log('event ' + contractName + event.get('name'))
           event.set('name', contractName + event.get('name'))
           console.log('after: ' + event.get('name'))
