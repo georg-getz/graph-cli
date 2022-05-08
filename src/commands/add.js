baeb1dfcdb0ba18dad15324423b743478bdf0318
georg-getz <georgi.getz@outlook.com>
2022-05-08 06:18:45 +0300

    debugging

--- src/commands/add.js
+++ src/commands/add.js
@@ -78,7 +78,8 @@ module.exports = {
         //   console.log('after: ' + event.get('name'))
         //   return event
         // });
-        for (lek in kek) {
+        for (let i = 0; i < kek.size(); i++) {
+          let lek = kek.get(i)
           console.log('lek: ' + lek)
           if (lek.get('type' === 'event')) {
             lek.set('name', contractName + lek.get('name'))
